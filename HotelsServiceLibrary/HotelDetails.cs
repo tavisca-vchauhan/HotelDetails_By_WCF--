578b804fb4b3397b1603e41c64f2d578227c5006
Vishwas Chauhan <vchauhan@tavisca.com>
2018-09-18 17:32:15 +0530

    services made by using WCF

---
+++ HotelsServiceLibrary/HotelDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelsServiceLibrary
+{
+    [DataContract]
+    public class HotelDetails
+    {
+        [DataMember]
+        public string HotelName { get; set; }
+        [DataMember]
+        public string HotelId { get; set; }
+        [DataMember]
+        public string Address { get; set; }
+        [DataMember]
+        public double StarRating { get; set; }
+    }
+
+}
---
+++ HotelsServiceLibrary/Hotels.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelsServiceLibrary
+{
+    public class Hotel : IHotelService
+    {
+
+        public string GetHotelById(int id)
+            {
+                using (StreamReader r = new StreamReader("../../HotelData.json"))
+                {
+                    string json = r.ReadToEnd();
+                    List<HotelDetails> hotels = JsonConvert.DeserializeObject<List<HotelDetails>>(json);
+
+                    for (int index = 0; index < hotels.Count; index++)
+                    {
+                        if (id == Convert.ToInt32(hotels[index].HotelId))
+                            return string.Format("Hotel Name is : {0}\nHotel ID is : {1}\nHotel address is : {2}\nHotel rating is : {3}", hotels[index].HotelName, hotels[index].HotelId, hotels[index].Address, hotels[index].StarRating);
+                    }
+                }
+                return string.Format("Wrong ID !");
+
+            }
+
+            public List<HotelDetails> GetHotelData()
+            {
+                using (StreamReader r = new StreamReader("../../HotelData.json"))
+                {
+                    string json = r.ReadToEnd();
+                    List<HotelDetails> hotels = JsonConvert.DeserializeObject<List<HotelDetails>>(json);
+                    return hotels;
+                }
+            }       
+    }
+
+
+
+}
